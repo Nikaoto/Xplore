f0db073f3a9e5c1750b34b5470bcc68ecb1878a6
gugakatsi <guga.katsiashvili@yahoo.com>
2017-07-18 19:15:42 +0400

    Settings finished, left SettingsFragment methods -> LanguageSettingAcivity/Fragment

--- app/src/main/java/com/xplore/settings/SettingsActivity.kt
+++ app/src/main/java/com/xplore/settings/SettingsActivity.kt
@@ -1,10 +1,14 @@
 package com.xplore.settings
 
+import android.content.Intent
 import android.os.Bundle
 import android.preference.PreferenceFragment
 import android.view.MenuItem
 
 import com.xplore.R
+import android.preference.Preference
+
+
 
 class SettingsActivity : AppCompatPreferenceActivity() {
 
@@ -12,7 +16,6 @@ class SettingsActivity : AppCompatPreferenceActivity() {
         super.onCreate(savedInstanceState)
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true)
-
         fragmentManager.beginTransaction().replace(android.R.id.content, MainPreferenceFragment()).commit()
     }
 
@@ -23,12 +26,13 @@ class SettingsActivity : AppCompatPreferenceActivity() {
             /*  */
             addPreferencesFromResource(R.xml.prefs_main)
 
-            /* Click Listeners for Log out , tutorial etc ... */
-
+            /* Click Listeners for Log out , tutorial etc , Language  ... */
+            val settings_button = findPreference("button_select_language")
+            settings_button.setOnPreferenceClickListener({
+                startActivity(Intent(activity,LanguageSettingsActivity::class.java))
+                true
+            })
         }
-
-        /* Handling language settings Preferences here */
-
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
