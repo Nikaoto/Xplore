2ffbf344fa8f11707ec03d4e107e05bc9a6302eb
Nika Otiashvili <nikaoto@gmail.com>
2017-07-29 15:58:50 +0400

    add splash screen, fix about page banner resolution

--- app/src/main/java/com/xplore/MainActivity.java
+++ app/src/main/java/com/xplore/MainActivity.java
@@ -67,6 +67,7 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme_NoActionBar);
         super.onCreate(savedInstanceState);
         InitPreferences(this);
         General.setCurrentTable(this);
