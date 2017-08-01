016dac441ef86b7c71431ed7c4ed825df502e80c
Nika Otiashvili <nikaoto@gmail.com>
2017-08-01 04:03:58 +0400

    add profile image picking from gallery and camera upon registration (TODO add upload to firebase storage)

--- app/src/main/java/com/xplore/account/RegisterActivity.kt
+++ app/src/main/java/com/xplore/account/RegisterActivity.kt
@@ -1,11 +1,20 @@
 package com.xplore.account
 
+import android.Manifest
 import android.app.Activity
+import android.app.AlertDialog
 import android.app.DatePickerDialog
 import android.content.Context
+import android.content.DialogInterface
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.support.v4.content.FileProvider
 import android.util.Log
 import android.widget.DatePicker
 import android.widget.EditText
@@ -13,9 +22,10 @@ import android.widget.Toast
 
 import com.google.firebase.database.Exclude
 import com.google.firebase.database.FirebaseDatabase
+import com.soundcloud.android.crop.Crop
 import com.squareup.picasso.Picasso
-import com.xplore.CircleTransformation
 import com.xplore.General
+import com.xplore.ImageUtil
 import com.xplore.R
 
 import java.util.HashMap
@@ -24,6 +34,8 @@ import com.xplore.TimeManager.Companion.globalTimeStamp
 import com.xplore.TimeManager.Companion.refreshGlobalTimeStamp
 import com.xplore.user.User
 import kotlinx.android.synthetic.main.register_layout.*
+import java.io.File
+import java.io.IOException
 
 /**
 * Created by Nikaoto on 3/11/2017.
@@ -31,6 +43,15 @@ import kotlinx.android.synthetic.main.register_layout.*
 */
 
 class RegisterActivity : Activity(), DatePickerDialog.OnDateSetListener {
+
+    /* Request Codes */
+    val NONE = 0
+    val CAMERA_PERMISSION_REQUEST_CODE = 1;
+    val GALLERY_PERMISSION_REQUEST_CODE = 2;
+    val ACTION_SNAP_IMAGE = 3
+
+    // File provider authority string
+    val authority = "com.xplore.fileprovider"
 
     //TODO add age restriction constant to resources
     private val ageRestriction: Int = 16
@@ -78,16 +99,23 @@ class RegisterActivity : Activity(), DatePickerDialog.OnDateSetListener {
             val fragment = com.xplore.CustomDatePicker(this, globalTimeStamp, ageRestriction)
             fragment.show(fragmentManager, "datePicker")
         }
-        //TODO CHOOSE GALLERY IMAGE OR TAKE PHOTO
         //TODO AI check for face in photo?
-
-        val userImageSize = Math.round(resources.getDimension(R.dimen.user_profile_image_medium_size))
 
         if(userProfilePicUrl != "") {
             Picasso.with(this@RegisterActivity)
                     .load(userProfilePicUrl)
-                    .transform(CircleTransformation(userImageSize, userImageSize))
+                    .transform(ImageUtil.mediumCircle(this))
                     .into(profileImageView)
+        }
+
+        profileImageView.setOnClickListener {
+            //TODO string resources
+            val dialog = AlertDialog.Builder(this).setTitle("Take picture from..")
+                    .setNegativeButton("Camera", takeFromCamera)
+                    .setPositiveButton("Gallery", takeFromGallery)
+                    .setNeutralButton(R.string.cancel, null)
+                    .create()
+            dialog.show()
         }
 
         doneButton.setOnClickListener {
@@ -115,10 +143,12 @@ class RegisterActivity : Activity(), DatePickerDialog.OnDateSetListener {
                 bDay = day
 
                 bdateTextView.text = "$bYear/${addZero(bMonth)}/${addZero(bDay)}"
-            } else
+            } else {
+                //TODO string resources
                 Toast.makeText(this@RegisterActivity,
                         "You must be of age $ageRestriction to use Xplore",
-                        Toast.LENGTH_SHORT).show() //TODO string resources
+                        Toast.LENGTH_SHORT).show()
+            }
         } else
             General.createNetErrorDialog(this@RegisterActivity)
     }
@@ -128,7 +158,6 @@ class RegisterActivity : Activity(), DatePickerDialog.OnDateSetListener {
 
         val childUpdates = HashMap<String, Any>()
         childUpdates.put("/users/" + user.uid, userData)
-        Log.println(Log.INFO, "BREJK", "uid: " + user.uid)
         DBref.updateChildren(childUpdates)
         setResult(Activity.RESULT_OK)
         finish()
@@ -189,4 +218,127 @@ class RegisterActivity : Activity(), DatePickerDialog.OnDateSetListener {
     }
 
     fun EditText.str() = this.text.toString() //TODO take this to general
+
+    /* Everything below is code needed for profile picture choosing or taking functionality */
+
+    var imagePath: Uri? = null
+
+    //Requests permissions for given module with the passed request code and permissionType
+    fun requestModulePermission(activity: Activity, permission: String, requestCode: Int,
+                                module: () -> Unit) {
+        //Checking if not allowed
+        if (ContextCompat.checkSelfPermission(activity, permission)
+                != PackageManager.PERMISSION_GRANTED) {
+            //Open dialogue requesting permission
+            Log.println(Log.INFO, "camera", "requesting permission")
+
+            ActivityCompat.requestPermissions(activity, arrayOf(permission), requestCode)
+        } else {
+            module()
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<out String>?,
+                                            grantResults: IntArray?) {
+        if (grantResults != null && grantResults.isNotEmpty()
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED ) {
+            when (requestCode) {
+                CAMERA_PERMISSION_REQUEST_CODE -> prepareCamera()
+                GALLERY_PERMISSION_REQUEST_CODE -> Crop.pickImage(this)
+            }
+        }
+    }
+
+    val takeFromCamera = DialogInterface.OnClickListener {
+        _, _ ->
+        Log.println(Log.INFO, "camera", "write_ext_storage permission")
+            requestModulePermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    CAMERA_PERMISSION_REQUEST_CODE, { prepareCamera() })
+
+    }
+
+    // Prepares extra permissions for camera and then opens it(kind of hacky)
+    fun prepareCamera() {
+        Log.println(Log.INFO, "camera", "preparing Camera")
+        Log.println(Log.INFO, "camera", "camera permission")
+        requestModulePermission(this, Manifest.permission.CAMERA,
+                CAMERA_PERMISSION_REQUEST_CODE, { openCamera() })
+    }
+
+    fun openCamera() {
+        Log.println(Log.INFO, "camera", "opening Camera")
+
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        if (takePictureIntent.resolveActivity(packageManager) != null) {
+            val photoFile: File
+            try {
+                photoFile = createImageFile();
+            } catch (ex: IOException) {
+                throw ex
+            }
+            val photoURI = FileProvider.getUriForFile(this, authority, photoFile)
+            imagePath = photoURI
+            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+            startActivityForResult(takePictureIntent, ACTION_SNAP_IMAGE);
+        }
+    }
+
+    val takeFromGallery = DialogInterface.OnClickListener {
+        _, _ ->
+            requestModulePermission(this, Manifest.permission.READ_EXTERNAL_STORAGE,
+                    GALLERY_PERMISSION_REQUEST_CODE, { Crop.pickImage(this) })
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (data != null) {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+
+        /* Picking or Taking picture */
+        when (requestCode) {
+            ACTION_SNAP_IMAGE -> {
+                if(resultCode == Activity.RESULT_OK && imagePath != null) {
+                    cropImage(imagePath as Uri, imagePath as Uri)
+                    addPictureToGallery(imagePath.toString())
+                }
+            }
+            Crop.REQUEST_PICK -> {
+                if (resultCode == Activity.RESULT_OK && data != null) {
+                    val photoURI = FileProvider.getUriForFile(this, authority, createImageFile())
+                    cropImage(data.data, photoURI)
+                }
+            }
+            Crop.REQUEST_CROP -> {
+                if (data != null) {
+                    Picasso.with(this)
+                            .load(Crop.getOutput(data))
+                            .transform(ImageUtil.mediumCircle(this))
+                            .into(profileImageView)
+                    imagePath = Crop.getOutput(data)
+                }
+            }
+        }
+    }
+
+    //Starts cropping activity with code Crop.REQUEST_CROP
+    fun cropImage(input: Uri, output: Uri) = Crop.of(input, output).asSquare().start(this)
+
+    // Returns temporary file for storing cropped picture
+    @Throws(IOException::class)
+    fun createImageFile(): File {
+        val imageFileName = "profile_pic_"
+        val storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        val imageFile = File.createTempFile(imageFileName, ".jpg", storageDir)
+        return imageFile
+    }
+
+    /* add picture to gallery */
+    private fun addPictureToGallery(picturePath: String) {
+        val mediaScanIntent = Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE)
+        val f = File(picturePath)
+        val contentUri = Uri.fromFile(f)
+        mediaScanIntent.data = contentUri
+        this.sendBroadcast(mediaScanIntent)
+    }
 }
