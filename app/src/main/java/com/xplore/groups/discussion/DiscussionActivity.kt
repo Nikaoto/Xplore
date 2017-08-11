b4e233b8912cdaa920486fb1fc6b58d6609beacb
Nika Otiashvili <nikaoto@gmail.com>
2017-08-11 22:34:27 +0400

    add discussion layout

--- app/src/main/java/com/xplore/groups/discussion/DiscussionActivity.kt
+++ app/src/main/java/com/xplore/groups/discussion/DiscussionActivity.kt
@@ -1,17 +1,41 @@
 package com.xplore.groups.discussion
 
 import android.app.Activity
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.xplore.R
 
 /**
  * Created by Nikaoto on 8/11/2017.
  * TODO write description of this class - what it does and why.
  */
+
 class DiscussionActivity : Activity() {
+
+    //Firebase
+    val F_GROUPS = "groups"
+    private lateinit var currentGroupRef: DatabaseReference
+    //
+    private lateinit var groupId: String
+
+    companion object {
+        @JvmStatic
+        fun getStartIntent(context: Context, groupId: String): Intent {
+            return Intent(context, DiscussionActivity::class.java)
+                    .putExtra("groupId", groupId)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView()
+
+        groupId = intent.getStringExtra("groupId")
+        currentGroupRef = FirebaseDatabase.getInstance().getReference("$F_GROUPS/$groupId")
+
+        setContentView(R.layout.discussion)
         startListeningForMessages()
     }
 
