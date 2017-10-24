2c8118f7cbf58d9574fc1a2cbb225b823a5c1358
Nika Otiashvili <nikaoto@gmail.com>
2017-10-24 21:02:24 +0400

    add onSearchClick, onSearchTextViewClick, onSearchViewClose to SearchFrag

--- app/src/main/java/com/xplore/base/SearchFragment.kt
+++ app/src/main/java/com/xplore/base/SearchFragment.kt
@@ -1,10 +1,13 @@
 package com.xplore.base
 
 import android.app.Fragment
-import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.*
+import android.widget.LinearLayout
+import android.widget.RelativeLayout
 import android.widget.SearchView
+import android.widget.TextView
 import com.xplore.R
 
 /**
@@ -16,24 +19,58 @@ import com.xplore.R
 
 open class SearchFragment : Fragment(), SearchView.OnQueryTextListener {
 
-    var searchView: SearchView? = null
+    private var searchView: SearchView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setHasOptionsMenu(true)
     }
 
+    // Sets onClickListener to given SearchView's TextView (recursively runs children down)
+    private fun setSearchViewTextOnClickListener(v: View, listener: View.OnClickListener) {
+        val group = v as ViewGroup
+        for (i in 0 until group.childCount) {
+            val child = group.getChildAt(i)
+            if (child is LinearLayout || child is RelativeLayout) {
+                setSearchViewTextOnClickListener(child, listener)
+            }
+
+            if (child is TextView) {
+                child.setOnClickListener(listener)
+            }
+        }
+    }
+
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.search, menu)
         val searchItem = menu.findItem(R.id.action_search) as MenuItem
         searchView = searchItem.actionView as SearchView
-        searchView?.setOnQueryTextListener(this)
+        searchView?.let {
+            it.setOnQueryTextListener(this)
+            it.setOnSearchClickListener { v -> onSearchClick(v) }
+            setSearchViewTextOnClickListener(it,
+                    View.OnClickListener { v -> onSearchTextViewClick(v) })
+            it.setOnCloseListener { onSearchViewClose(); false}
+        }
+
         setUpSearchView(searchView)
         super.onCreateOptionsMenu(menu, inflater)
     }
 
+    open fun onSearchClick(v: View) {
+        // Called when search icon is clicked
+    }
+
+    open fun onSearchTextViewClick(v: View) {
+        // Called when the TextView inside the SearchView is clicked
+    }
+
+    open fun onSearchViewClose() {
+        // Called when SearchView is closed by the little 'X'
+    }
+
     open fun setUpSearchView(newSearchView: SearchView?) {
-        // Here we set up the searchview and customize it
+        // Here we set up the SearchView and customize it
         newSearchView?.queryHint = resources.getString(R.string.search_hint)
     }
 
