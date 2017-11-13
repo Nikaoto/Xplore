cbf11cc07f424ad7ef82991eb208bce9fe366839
desire-rawr <guga.katsiashvili@gmail.com>
2017-11-13 23:08:46 +0400

    datepicker dialog

---
+++ app/src/main/java/com/xplore/BirthDatePickerFragment.kt
@@ -0,0 +1,28 @@
+package com.xplore
+
+import android.annotation.SuppressLint
+import android.widget.DatePicker
+import android.app.DatePickerDialog
+import android.app.Dialog
+import android.app.DialogFragment
+import android.os.Bundle
+import java.util.*
+
+
+@SuppressLint("ValidFragment")
+class BirthDatePickerFragment(private val onDataSet: (view: DatePicker, year: Int, month: Int, day: Int) -> Unit)
+    : DialogFragment(), DatePickerDialog.OnDateSetListener {
+
+    override fun onCreateDialog(savedInstanceState: Bundle): Dialog {
+        // Use the current date as the default date in the picker
+        val c = Calendar.getInstance()
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val day = c.get(Calendar.DAY_OF_MONTH)
+
+        return DatePickerDialog(activity, this, year, month, day)
+    }
+
+    override fun onDateSet(view: DatePicker, year: Int, month: Int, day: Int) = onDataSet(view, year, month, day)
+
+}
