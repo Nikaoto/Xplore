6b08ba83e1ac4adea66a1947f39407c856674127
Nika Otiashvili <nikaoto@gmail.com>
2017-12-08 01:38:12 +0400

    remove live hike func from GroupMapAct

--- app/src/main/java/com/xplore/maps/GroupMapActivity.kt
+++ app/src/main/java/com/xplore/maps/GroupMapActivity.kt
@@ -34,12 +34,10 @@ import com.xplore.util.MapUtil
 
 class GroupMapActivity : BaseMapActivity() {
 
-    private val TAG = "gmap"
-    private val NO_GROUP_ID = " "
+    private val TAG = "group-map-act"
 
     companion object {
         //Arguments
-        private const val ARG_GROUP_ID = "groupId"
         private const val ARG_DESTINATION_NAME = "destinationName"
         private const val ARG_DESTINATION_LAT = "destinationLat"
         private const val ARG_DESTINATION_LNG = "destinationLng"
@@ -62,28 +60,6 @@ class GroupMapActivity : BaseMapActivity() {
                            destinationLat: Double, destinationLng: Double, markerHue: Float)
                 : Intent = getStartIntent(context, zoomToDestination, destinationName,
                 destinationLat, destinationLng).putExtra(ARG_MARKER_HUE, markerHue)
-
-        //When opening group
-        @JvmStatic
-        fun getStartIntent(context: Context, zoomToDestination: Boolean, groupId: String,
-                           destinationName: String, destinationLat: Double,
-                           destinationLng: Double): Intent {
-            return getStartIntent(context, zoomToDestination, destinationName, destinationLat,
-                    destinationLng)
-                    .putExtra(ARG_GROUP_ID, groupId)
-        }
-    }
-
-    // Passed variables
-    private val groupId: String by lazy {
-        getPassedGroupId()
-    }
-    private fun getPassedGroupId(): String {
-        if (intent.getStringExtra(ARG_GROUP_ID) == null) {
-            return NO_GROUP_ID
-        } else {
-            return intent.getStringExtra(ARG_GROUP_ID)
-        }
     }
 
     private val destinationLocation: LatLng by lazy {
@@ -100,65 +76,17 @@ class GroupMapActivity : BaseMapActivity() {
         intent.getFloatExtra(ARG_MARKER_HUE, MapUtil.DEFAULT_MARKER_HUE)
     }
 
-    private val groupLocationsRef: DatabaseReference by lazy {
-        getGroupRef(groupId).child(F_LOCATIONS)
-    }
-    private val currentUserLocationRef: DatabaseReference by lazy {
-        groupLocationsRef.child(General.currentUserId)
-    }
-
     private lateinit var locationUpdater: LocationUpdater
-
 
     // Markers for member tracking
     private val mapMarkers = HashMap<String, Marker>()
     // Holds references to each members' location so we can disable them OnDestroy()
     private val listenerMap = HashMap<String, ChildEventListener>()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        if (groupId != NO_GROUP_ID) {
-            firstUploadData()
-        }
-
-        locationUpdater = LocationUpdater(this,
-                LocationRequest().setInterval(5000L)
-                        .setFastestInterval(1000L)
-                        .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY),
-                { locationResult ->  uploadLocation(locationResult.lastLocation) }
-        )
-    }
-
     override fun onStartLocationUpdates() {
         super.onStartLocationUpdates()
 
         locationUpdater.start()
-    }
-
-    /* Uploads a UserCard to this user's location node if it doesn't exist.
-           When a new user joins a group, a new node isn't created, so we upload a UserMarker in the
-           locations node for the first time */
-    private fun firstUploadData() {
-        currentUserLocationRef.addListenerForSingleValueEvent(object : ValueEventListener {
-            override fun onDataChange(locationSnapshot: DataSnapshot?) {
-                if (locationSnapshot == null || !locationSnapshot.hasChild(F_GROUP_NAME)) {
-                    getCurrentUserRef().addListenerForSingleValueEvent(object : ValueEventListener {
-                        override fun onDataChange(dataSnapshot: DataSnapshot?) {
-                            dataSnapshot?.let {
-                                val fname = it.child(F_GROUP_NAME).getValue(String::class.java)
-                                if (fname != null) {
-                                    currentUserLocationRef.setValue(UserMarker(fname))
-                                }
-                            }
-                        }
-
-                        override fun onCancelled(p0: DatabaseError?) {}
-                    })
-                }
-            }
-
-            override fun onCancelled(p0: DatabaseError?) {}
-        })
     }
 
     private fun buildDestinationMarker(): MarkerOptions {
@@ -185,111 +113,13 @@ class GroupMapActivity : BaseMapActivity() {
 
         // Zoom to destination
         if (zoomToDestination) {
-            //Move camera to destination
-            val cameraUpdate = CameraUpdateFactory.newLatLngZoom(destinationLocation,
-                    ZOOM_AMOUNT)
+            val cameraUpdate = CameraUpdateFactory.newLatLngZoom(destinationLocation, ZOOM_AMOUNT)
             googleMap.animateCamera(cameraUpdate)
-        }
-
-        if (groupId != NO_GROUP_ID) {
-            startListeningForGroupLocations(googleMap)
-        }
-    }
-
-    private fun uploadLocation(location: Location) {
-        if (groupId != NO_GROUP_ID) {
-            currentUserLocationRef.child(F_LATITUDE).setValue(location.latitude)
-            currentUserLocationRef.child(F_LONGITUDE).setValue(location.longitude)
-        }
-    }
-
-    // Sets up listeners for member locations
-    private fun startListeningForGroupLocations(googleMap: GoogleMap) {
-        groupLocationsRef.addListenerForSingleValueEvent(object : ValueEventListener {
-            override fun onDataChange(dataSnapshot: DataSnapshot?) {
-                dataSnapshot?.let {
-                    it.children.forEach { markerSnapshot ->
-                        val key = markerSnapshot.key
-
-                        if (key != General.currentUserId) {
-                            val marker = markerSnapshot.getValue(UserMarker::class.java)
-
-                            if (marker != null) {
-                                // Creating listener for member
-                                val listener = object : ChildEventListener {
-                                    // Creates new UserMarker from data and puts it into the hashmap
-                                    override fun onChildAdded(data: DataSnapshot?, p1: String?) {
-                                        Log.i(TAG, "added child")
-                                        val mo = MarkerOptions()
-                                        mo.title(marker.name)
-                                        mo.position(LatLng(marker.latitude, marker.longitude))
-                                        mo.icon(BitmapDescriptorFactory.defaultMarker(marker.hue))
-
-                                        if (!mapMarkers.containsKey(key)) {
-                                            // Add marker if it's new
-                                            mapMarkers.put(key, googleMap.addMarker(mo))
-                                        } else {
-                                            // Update position
-                                            mapMarkers[key]?.position = mo.position
-                                        }
-                                    }
-
-                                    //Updates markers when data changes
-                                    override fun onChildChanged(data: DataSnapshot?, p1: String?) {
-                                        data?.let {
-                                            it.value?.let {
-                                                if (it is Double) {
-                                                    if (data.key == F_LATITUDE) {
-                                                        val lng = mapMarkers[key]?.position?.longitude
-                                                        mapMarkers[key]?.position= LatLng(it, lng!!)
-                                                    } else if (data.key == F_LONGITUDE) {
-                                                        val lat = mapMarkers[key]?.position?.latitude
-                                                        mapMarkers[key]?.position= LatLng(lat!!, it)
-                                                    }
-                                                } else if (it is UserMarker) {
-                                                    mapMarkers[key]?.position = it.getLocation()
-                                                }
-                                            }
-                                        }
-                                    }
-
-                                    override fun onChildMoved(p0: DataSnapshot?, p1: String?) {}
-
-                                    override fun onCancelled(p0: DatabaseError?) {}
-
-                                    override fun onChildRemoved(p0: DataSnapshot?) {}
-                                }
-                                startListeningForMemberLocation(key, listener)
-                            }
-                        }
-                    }
-                }
-            }
-
-            override fun onCancelled(p0: DatabaseError?) {}
-        })
-    }
-
-    private fun startListeningForMemberLocation(uId: String, listener: ChildEventListener) {
-        listenerMap.put(uId, listener)
-        groupLocationsRef.child(uId).addChildEventListener(listenerMap[uId])
-        /*TODO this^:
-         looks dumb and probably is dumb, but needs testing to confirm that in
-         OnDestroy() the correct listeners are removed (referenced from the map and not the method
-         argument) */
-    }
-
-    private fun stopListeningForMemberLocations() {
-        if (listenerMap.isNotEmpty()) {
-            listenerMap.forEach { entry ->
-                groupLocationsRef.child(entry.key).removeEventListener(entry.value)
-            }
         }
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        stopListeningForMemberLocations()
         stopLocationUpdates()
     }
 
