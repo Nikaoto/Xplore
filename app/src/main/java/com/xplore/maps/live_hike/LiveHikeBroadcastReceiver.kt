3ddc36987225957e35a1bff511849ffa28ce36e5
Nika Otiashvili <nikaoto@gmail.com>
2017-12-09 23:33:49 +0400

    rename LocationUpdateBroadcastReceiver -> LiveHikeBroadcastReceiver and move it to live_hike package; add uploadLocation func

--- app/src/main/java/com/xplore/maps/live_hike/LiveHikeBroadcastReceiver.kt
+++ app/src/main/java/com/xplore/maps/live_hike/LiveHikeBroadcastReceiver.kt
@@ -1,18 +1,24 @@
-package com.xplore.maps
+package com.xplore.maps.live_hike
 
 import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.location.Location
 import android.util.Log
 import com.google.android.gms.location.LocationResult
+import com.google.firebase.database.FirebaseDatabase
+import com.xplore.util.FirebaseUtil
 
 /**
  * Created by Nika on 12/2/2017.
- * TODO write description of this class - what it does and why.
+ *
+ * BroadcastReceiver class for background location updates during a Live Hike.
+ * Uploads Location data to given firebase node.
+ *
  */
 
-class LocationUpdateBroadcastReceiver : BroadcastReceiver() {
+class LiveHikeBroadcastReceiver(): BroadcastReceiver() {
 
     private val TAG = "LU-broadcast-receiver"
     private fun log(s: String) = Log.i(TAG, s)
@@ -21,12 +27,14 @@ class LocationUpdateBroadcastReceiver : BroadcastReceiver() {
         const val ACTION_PROCESS_UPDATES = "com.xplore.maps.action.PROCESS_UPDATES"
 
         @JvmStatic
-        fun getPendingIntent(context: Context): PendingIntent {
-            val intent = Intent(context, LocationUpdateBroadcastReceiver::class.java)
+        fun newPendingIntent(context: Context): PendingIntent {
+            val intent = Intent(context, LiveHikeBroadcastReceiver::class.java)
             intent.action = ACTION_PROCESS_UPDATES
             return PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
         }
     }
+
+
 
     override fun onReceive(context: Context?, intent: Intent?) {
         log("onReceive")
@@ -38,12 +46,24 @@ class LocationUpdateBroadcastReceiver : BroadcastReceiver() {
                 if (result != null) {
                     val locations = result.locations
 
+
                     locations.forEach { loc ->
                         log("lat: " + loc.latitude)
                         log("lng: " + loc.longitude)
                     }
+
+                   /* val userId = intent.getStringExtra("userId")
+                    val groupId = intent.getStringExtra("groupId")
+                    val ref = "/groups/$groupId/locations/$userId"
+
+                    uploadLocation(ref, result.lastLocation)*/
                 }
             }
         }
+    }
+
+    private fun uploadLocation(locationRef: String, location: Location) {
+        FirebaseDatabase.getInstance().getReference(locationRef).child("latitude").setValue(location.latitude)
+        FirebaseDatabase.getInstance().getReference(locationRef).child("longitude").setValue(location.longitude)
     }
 }
