addab5c80e353823275301570d7dc4e227ab4da5
Nika Otiashvili <nikaoto@gmail.com>
2017-12-19 20:47:18 +0400

    remove useless comment

--- app/src/main/java/com/xplore/groups/Group.java
+++ app/src/main/java/com/xplore/groups/Group.java
@@ -21,7 +21,7 @@ public class Group {
     public HashMap<String, Boolean> member_ids;
     public HashMap<String, Boolean> invited_member_ids;
     public double meetup_latitude, meetup_longitude;
-    //public double finish_latitude, finish_longitude;
+
 
     public Group() {
         //LEAVE EMPTY
